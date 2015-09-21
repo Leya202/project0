edc57b8eed8363e1a26ac2cd81cce210c669661a
Leya202 <leya202@gmail.com>
2015-09-21 15:38:46 -0400

    Update ProjectZero.java

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -4,19 +4,63 @@ public class ProjectZero {
 
 	public static void main(String[] args) {
 		
-			Scanner inputSource = new Scanner(System.in);
-			int input1, input2, result;
-			
-			System.out.print("Please enter two numbers of your exalted choosing: ");
-			input1 = inputSource.nextInt();
-			input2 = inputSource.nextInt();
-			
-			result = input1 + input2;
-			System.out.println(result);
-			
-			inputSource.close();
-			
-	}
+		Scanner inputSource = new Scanner(System.in);
+		double result = 0;
+
+		System.out.print("Please enter two numbers of your exalted choosing: ");
+		
+		String [] input = {"", ""};
+		input[0] = inputSource.next();
+		input[1] = inputSource.next();
+		
+		int [] arrInt = {0, 0};
+		double [] arrDouble = {0.0, 0.0};
+		
+		for (int i = 0; i < 2; i++)
+		{
+			if(isStringInt(input[i]))
+			{
+				arrInt[i] = Integer.parseInt(input[i]);
+				result += arrInt[i];
+			}
+			else if(isStringDouble(input[i]))
+			{
+				arrDouble[i] = Double.parseDouble(input[i]);
+				result = result + arrDouble[i];
+			}
+			else
+			{
+				System.out.println("I can only add numbers. " + input[i] + " is not a number, you can not fool me!");
+			}
+		}
+
+		System.out.println(result);
+		
+		inputSource.close();
+		
+}
+	
+public static boolean isStringInt(String s)
+{
+    try
+    {
+        Integer.parseInt(s);
+        return true;
+    } catch (NumberFormatException ex)
+    {
+        return false;
+    }
+}
+
+public static boolean isStringDouble(String str) {
+    try {
+        Double.parseDouble(str);
+        return true;
+    } catch (NumberFormatException e) {
+        return false;
+    }
+}
+
 
 }
 
